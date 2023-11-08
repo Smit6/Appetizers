56155a6cc501f8703ac4d5b689e1681004c117cd
Smitkumar Contractor <smitcontractor@gmail.com>
2023-11-08 09:58:35 -0800

    Initial commit

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Smitkumar Contractor on 11/8/23.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Smitkumar Contractor on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
